212874a426a3412f42402b12f21270521be3978b
rushi3691 <rushi3691@outlook.com>
2023-10-27 20:07:37 +0530

    add graphql server code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./graphql/schema";
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
